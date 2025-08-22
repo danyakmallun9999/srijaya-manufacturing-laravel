4ab5791cc72883511dd75d0bc494cb9381aedaf6
danyakmallun9999 <danyakmallun9999@gmail.com>
2025-08-22 19:34:50 +0700

    feat: ubah route

--- routes/web.php
+++ routes/web.php
@@ -40,11 +40,11 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('orders', OrderController::class);
 
     // Route untuk update status order
-    Route::patch('/orders/{order}/status', [OrderController::class, 'updateStatus'])->name('orders.updateStatus');
+    // Route::patch('/orders/{order}/status', [OrderController::class, 'updateStatus'])->name('orders.updateStatus');
     Route::patch('/orders/{order}/price', [OrderController::class, 'updatePrice'])->name('orders.updatePrice');
 
     // Route untuk menambah item BOM ke order
-    Route::post('/orders/{order}/boms', [OrderController::class, 'storeBomItem'])->name('orders.boms.store');
+    // Route::post('/orders/{order}/boms', [OrderController::class, 'storeBomItem'])->name('orders.boms.store');
 
     // Route untuk menghapus item BOM dari order
     Route::delete('/orders/{order}/boms/{orderBom}', [OrderController::class, 'destroyBomItem'])->name('orders.boms.destroy');
